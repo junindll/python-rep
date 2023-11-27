583cdcad59da084c1cc2955062f1fc5cd70e03c8
Júnior <146141481+junindll@users.noreply.github.com>
2023-11-27 20:18:53 -0300

    Add files via upload

---
+++ Formatação básica de strings.py
@@ -0,0 +1,17 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de digitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< -  Direita
+^ - Centro
+Sinal - + ou -
+Ex.:0>-100,.1f
+conversion flags - lr ls la
+"""
+variavel = 'ABC'
+print(variavel.format(>10))
